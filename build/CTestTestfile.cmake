529d5379620a47081896cbd702f02485bd6d7238
Diego Ojeda <daom89@gmail.com>
2022-09-03 11:31:30 -0500

    Ambiente 03-09-2022

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,18 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros/catkin_ws/src
+# Build directory: /home/ros/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlebot3/turtlebot3")
+subdirs("turtlebot3/turtlebot3_navigation")
+subdirs("curso_ros")
+subdirs("turtlebot3/turtlebot3_bringup")
+subdirs("turtlebot3/turtlebot3_example")
+subdirs("turtlebot3_gazebo")
+subdirs("turtlebot3/turtlebot3_slam")
+subdirs("turtlebot3/turtlebot3_teleop")
+subdirs("robotv3")
+subdirs("world_v3")
+subdirs("turtlebot3/turtlebot3_description")
---
+++ build/world_v3/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros/catkin_ws/src/world_v3
+# Build directory: /home/ros/catkin_ws/build/world_v3
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
